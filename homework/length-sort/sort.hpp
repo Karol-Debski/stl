e386a8063b3fa83b026f4850fa9868acdbf0bc08
karol <karol3129@gmail.com>
2025-06-19 00:28:19 +0200

    Added sort implementation

---
+++ homework/length-sort/sort.hpp
@@ -0,0 +1,10 @@
+#ifndef SORT_HPP
+#define SORT_HPP
+
+#include <deque>
+#include <string>
+#include <forward_list>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string>& fl);
+
+#endif
